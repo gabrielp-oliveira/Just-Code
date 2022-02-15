53408af1af6d2e588207f6692b70f0fc9059734c
~Gabriel <~gabriel.p1997@yahoo.com~>
2022-02-15 12:35:05 -0500

    ex2

--- codes/ex1.js
+++ codes/ex1.js
@@ -1,49 +1,50 @@
-
-// fetch('https://api.github.com/search/users?q=gabrielp-oliveira')
-//     .then(resp => resp.text())
-//     .then(html => console.log(html))
-
-function getUsers(e){
-    if(e.value.trim() != '' && e.value.length >= 4){
+function getUsers(e) {
+    if (e.value.trim() != '' && e.value.length >= 4) {
 
         fetch(`https://api.github.com/search/users?q=${e.value}`)
             .then(html => html.json().then((data) => {
                 const list = document.querySelector('#listUsers')
                 while (list.firstChild) {
                     list.removeChild(list.firstChild);
-                  }
-                  
+                }
+
                 data.items.forEach(element => {
+
                     const container = document.createElement('div')
-                    container.setAttribute('info', element.login )
-                    container.addEventListener('click', () => {
-                        eventClick(element.login, element.subscriptions_url)
+                    const login = document.createElement('h3')
+                    const profileButton = document.createElement('button')
+                    profileButton.addEventListener('click', () => {
+                        eventClick(element.login)
                     })
-                    
+                    profileButton.innerHTML = 'contributions'
+                    login.innerHTML = element.login
+                    const id = document.createElement('div')
+                    id.append(login)
+                    container.setAttribute('info', element.login)
+
                     const image = document.createElement('img')
                     image.style.height = '32px'
                     image.style.width = '32px'
                     image.src = element.avatar_url
                     container.append(image)
+                    container.append(id)
+                    container.append(profileButton)
                     list.append(container)
-                    
-                });
+
+                })
             }))
     }
 
 }
-function eventClick(ev, apiUrl){
+function eventClick(ev) {
     const url = `https://ghchart.rshah.org/${ev}`
     const image = document.createElement('img')
     image.src = url
-    const container = document.querySelector('#graphResult')
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+    const graphResult = document.querySelector('#graphResult')
+    graphResult.className = "ex1ContainerUser"
+    while (graphResult.firstChild) {
+        graphResult.removeChild(graphResult.firstChild);
     }
-    container.append(image)
-
-    fetch(apiUrl)
-    .then((a) => a.json())
-    .then((a) => console.log(a))
+    graphResult.append(image)
 
 }
---
+++ codes/ex2.js
@@ -0,0 +1,42 @@
+function newTodoItem(){
+    let inputValue = document.getElementById("todoInput");
+    let todoList = document.getElementById("todoList")
+    let sit = false
+    todoList.childNodes.forEach((a) => {
+        if(a.firstChild.innerHTML == inputValue.value){
+            alert("todo already cadastred");
+            sit = true
+            return 
+        }
+    })
+    if(sit){
+        return
+    }
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    let close = document.createElement("div");
+    close.innerHTML = 'x'
+    close.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    div.addEventListener('click', function(ev) {
+        if (div.className == 'checked') {
+            div.className = ''
+        }else{
+            div.className = 'checked'
+        }
+      })
+    
+    if (inputValue.value.trim() === '') {
+        alert("You must write something!");
+        return
+    } else {
+        div.innerHTML = inputValue.value
+        li.append(div)
+        li.append(close)
+        todoList.appendChild(li);
+    }
+    inputValue.value = ''
+    console.log(inputValue.value)
+}
